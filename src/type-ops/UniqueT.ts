f9078750245205f0a66553ee25e8e162774bcf31
Alexey Gorishny <0x414c@gmail.com>
2018-07-11 01:02:37 +0300

    fix(UniqueT): add constraint to `TTag'

--- src/type-ops/UniqueT.ts
+++ src/type-ops/UniqueT.ts
@@ -11,7 +11,7 @@ export declare const TYPE_TAG: unique symbol;
 /**
  * Make an opaque alias of `T` using tag `TTag`.
  */
-export type UniqueT<T, TTag> = T
+export type UniqueT<T, TTag extends PropertyKey> = T
     & {
       [RAW_TYPE]: T;
       [TYPE_TAG]: TTag;
